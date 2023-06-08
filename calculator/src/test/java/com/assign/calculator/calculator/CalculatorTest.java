e63b0119112bf370daecba90da39df57f407e81a
Shuchitha01 <131276027+Shuchitha01@users.noreply.github.com>
2023-06-08 14:54:38 +0530

    Add files via upload

---
+++ calculator/src/test/java/com/assign/calculator/calculator/CalculatorTest.java
@@ -0,0 +1,39 @@
+package com.assign.calculator.calculator;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+  
+  @Test
+  public void testSum() {
+    int result = Calculator.sum(2, 3);
+    assertEquals(5, result);
+  }
+  @Test
+  public void testsubtract(){
+  int result=Calculator.subtract(10,3);
+  assertEquals(7,result);
+  }
+  @Test 
+  public void testmultiply() {
+  	int result=Calculator.multiply(5, 5);
+  	assertEquals(25,result);
+  }
+  @Test 
+  public void testdivide() {
+  	int result=Calculator.divide(10, 5);
+  	assertEquals(2,result);
+  	
+
+  }
+
+  }
+ 
+
+  
+  
+  
+  
+  
+
